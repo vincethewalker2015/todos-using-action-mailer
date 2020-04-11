c4fedf8c42a6d19078190cec71eb3f8f71b21cdf
Vincent <vincethewalker@yahoo.co.uk>
2020-04-11 19:09:13 +0100

    Added Bootstrap css

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const images = require.context('../../assets/images', true)
+const webpack = require("webpack") 
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+$: 'jquery',
+jQuery: 'jquery',
+Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
