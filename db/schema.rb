8d40f14106c89fa26f6119de1bb32f7ac8ac6b20
Vincent <vincethewalker@yahoo.co.uk>
2020-04-12 13:21:26 +0100

    Completed User profile togeather with log-in and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_182346) do
+ActiveRecord::Schema.define(version: 2020_04_12_105418) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_182346) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
