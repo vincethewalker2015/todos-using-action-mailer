278b9d63e97f679ef305107078226f825c3c9145
Vincent <vincethewalker@yahoo.co.uk>
2020-04-12 11:47:37 +0100

    Added user profile part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_131923) do
+ActiveRecord::Schema.define(version: 2020_04_11_182346) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
